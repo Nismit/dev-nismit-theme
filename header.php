896e730fec2bfda1cdfad5770ac277cafb700aac
Nismit <nismit.dev@gmail.com>
2017-07-03 02:40:08 -0700

    Set absolute path of favicon

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
-<link rel="icon" href="favicon.ico">
+<link rel="icon" href="<?php echo esc_url( home_url( '/' ) ); ?>favicon.ico">
 <?php wp_head(); ?>
 </head>
 
