b2d534ae8e217fab89afecb8146258a797c5c2fd
Nismit <nismit.dev@gmail.com>
2017-07-03 17:01:14 -0700

    Added sidebar for share

--- sidebar-article.php
+++ sidebar-article.php
@@ -11,13 +11,7 @@
 <section class="block">
 	<h2 class="block__title">Share</h2>
 
-	<ul class="nav nav--share">
-		<li class="nav__item nav--share__item"><a href="#" class="button button--share button--twitter"><svg class="icon icon--share icon--white"><use xlink:href="#icon-twitter"/></svg><span>&nbsp;Twitter</span><span class="counter">123</span></a></li>
-		<li class="nav__item nav--share__item"><a href="#" class="button button--share button--facebook"><svg class="icon icon--share icon--white"><use xlink:href="#icon-facebook"/></svg><span>&nbsp;Facebook</span><span class="counter">12</span></a></li>
-		<li class="nav__item nav--share__item"><a href="#" class="button button--share button--pocket"><svg class="icon icon--share icon--white"><use xlink:href="#icon-get-pocket"/></svg><span>&nbsp;Pocket</span><span class="counter">12</span></a></li>
-		<li class="nav__item nav--share__item"><a href="#" class="button button--share button--hatena"><svg class="icon icon--share icon--white"><use xlink:href="#icon-hatena"/></svg><span>&nbsp;Hatena</span><span class="counter">12</span></a></li>
-		<li class="nav__item nav--share__item"><a href="#" class="button button--share button--feedly"><svg class="icon icon--share icon--white"><use xlink:href="#icon-feedly"/></svg><span>&nbsp;Feedly</span><span class="counter">12</span></a></li>
-	</ul>
+	<?php get_sidebar('share'); ?>
 </section>
 
 <section class="block">
--- template-parts/content.php
+++ template-parts/content.php
@@ -31,12 +31,10 @@
 	</figure>
 
 	<div>
-		<ul class="nav nav--share">
-			<li class="nav__item nav--share__item"><a href="#" class="button button--share button--twitter"><svg class="icon icon--share icon--white"><use xlink:href="#icon-twitter"/></svg><span>&nbsp;Twitter</span><span class="counter">123</span></a></li><li class="nav__item nav--share__item"><a href="#" class="button button--share button--facebook"><svg class="icon icon--share icon--white"><use xlink:href="#icon-facebook"/></svg><span>&nbsp;Facebook</span><span class="counter">12</span></a></li><li class="nav__item nav--share__item"><a href="#" class="button button--share button--pocket"><svg class="icon icon--share icon--white"><use xlink:href="#icon-get-pocket"/></svg><span>&nbsp;Pocket</span><span class="counter">12</span></a></li><li class="nav__item nav--share__item"><a href="#" class="button button--share button--hatena"><svg class="icon icon--share icon--white"><use xlink:href="#icon-hatena"/></svg><span>&nbsp;Hatena</span><span class="counter">12</span></a></li><li class="nav__item nav--share__item"><a href="#" class="button button--share button--feedly"><svg class="icon icon--share icon--white"><use xlink:href="#icon-feedly"/></svg><span>&nbsp;Feedly</span><span class="counter">12</span></a></li>
-		</ul>
+		<?php get_sidebar('share'); ?>
 	</div>
 
-	<div class="table-contents">
+	<!--div class="table-contents">
 		<h2 class="table-contents__title">目次</h2>
 		<ol>
 			<li><a href="#">PHPアクセラレーション等を駆使してWordPressの高速化を図る</a></li>
@@ -44,18 +42,20 @@
 			<li><a href="#">PHPアクセラレーション等を駆使してWordPressの高速化を図る</a></li>
 			<li><a href="#">PHPアクセラレーション等を駆使してWordPressの高速化を図る</a></li>
 		</ol>
-	</div>
+	</div-->
 
 	<div class="sponsored">
 		<p class="u-text-center">Sponsored</p>
-		<div style="height: 120px; background-color: #ccc;"></div>
+		<div style="height: 120px; background-color: #ccc;">
+
+		</div>
 	</div>
 
 	<div class="post__content">
-		<div class="notice">
+		<!--div class="notice">
 			<p>この記事は最終更新日から1年以上経過しています。<br>
 			古くなっている場合、コメントもしくはTwitterまでご連絡いただけると幸いです。</p>
-		</div>
+		</div-->
 
 		<?php the_content(); ?>
 
